79fcbf0a13de9225085ac40a2ac2e1cb711598c3
andrea_artuso <andrea.artuso.business@gmail.com>
2025-08-26 22:00:27 +0200

    Create subdirectories

---
+++ scripts/post-install-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 1. Run PVE post-install script
+echo "Running post-install script\n"
+echo "DO NOT REBOOT WHEN PROMPTED\n"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/pve/post-pve-install.sh)"
+
+# 2. Network configuration
+echo "Importing network config...\n"
+wget -O /etc/pve/interfaces.new https://raw.githubusercontent.com/andrea-artuso/homelab/refs/heads/main/etc-network-interfaces.txt
+echo "Check network config...\n"
+ifup -a -i /etc/pve/interfaces.new --no-act
+echo "Applying network config...\n"
+mv /etc/pve/interfaces.new /etc/pve/interfaces
+ifreload -a
+
+# 3. Setup NGINX Proxy
+apt install nginx
+rm /etc/nginx/sites-enabled/default
+wget -O /etc/nginx/conf.d/proxmox.conf https://raw.githubusercontent.com/andrea-artuso/homelab/refs/heads/main/pve-nginx-proxy.conf
+nginx -t
+systemctl restart nginx
+
+# 4. Reboot system
+echo "Rebooting now...\n"
+reboot
