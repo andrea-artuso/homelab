cdf01a479244d055564081d242b25bbe30784190
andrea_artuso <andrea.artuso.business@gmail.com>
2025-08-26 22:32:17 +0200

    General fixes

--- scripts/post-install-script.sh
+++ scripts/post-install-script.sh
@@ -7,18 +7,28 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/Proxmo
 
 # 2. Network configuration
 echo "Importing network config...\n"
-wget -O /etc/pve/interfaces.new https://raw.githubusercontent.com/andrea-artuso/homelab/refs/heads/main/config/etc-network-interfaces.txt 
+wget -O /etc/network/interfaces.new https://raw.githubusercontent.com/andrea-artuso/homelab/refs/heads/main/config/etc-network-interfaces.txt 
 echo "Check network config...\n"
-ifup -a -i /etc/pve/interfaces.new --no-act
+ifup -a -i /etc/network/interfaces.new --no-act
 echo "Applying network config...\n"
-mv /etc/pve/interfaces.new /etc/pve/interfaces
+mv /etc/network/interfaces.new /etc/network/interfaces
 ifreload -a
 
 # 3. Setup NGINX Proxy
+## Install NGINX
 apt install nginx
 rm /etc/nginx/sites-enabled/default
+## Import config
 wget -O /etc/nginx/conf.d/proxmox.conf https://raw.githubusercontent.com/andrea-artuso/homelab/refs/heads/main/config/pve-nginx-proxy.conf
 nginx -t
+## Set service startup priority (after pve-cluster for certs)
+mkdir -p /etc/systemd/system/nginx.service.d
+cat > /etc/systemd/system/nginx.service.d/override.conf << 'EOF'
+[Unit]
+Requires=pve-cluster.service
+After=pve-cluster.service
+EOF
+systemctl daemon-reload
 systemctl restart nginx
 
 # 4. Reboot system
